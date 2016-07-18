0787897ec783da93950b83b7759df516accb54ff
pgmac <pk87654@163.com>
2016-07-18 22:43:16 +0800

    add class

---
+++ demo/src/java/cn.ej.service/StartController.java
@@ -0,0 +1,12 @@
+package cn.ej.service;
+
+/**
+ * Created by pgmac on 16/7/18.
+ */
+public class StartController {
+    public void test() {
+        System.out.println("StartController.test");
+    }
+
+
+}
